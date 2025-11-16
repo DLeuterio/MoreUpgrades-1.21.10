c21d6d4549a97c4f9c57597bd871649ec59f6ae0
DLeuterio6 <153567172+DLeuterio@users.noreply.github.com>
2025-11-17 02:35:05 +0800

    added comment test for commit

--- src/main/java/net/dawmii/moreupgrades/MoreUpgrades.java
+++ src/main/java/net/dawmii/moreupgrades/MoreUpgrades.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 public class MoreUpgrades implements ModInitializer {
 	public static final String MOD_ID = "moreupgrades";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    //Test comment for commit
 
 	@Override
 	public void onInitialize() {
