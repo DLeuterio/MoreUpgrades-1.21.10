cd9e8d33bc88dcaa516af15de5ca5725dc06a541
DLeuterio6 <153567172+DLeuterio@users.noreply.github.com>
2025-11-17 02:23:52 +0800

    Initial commit

---
+++ src/main/java/net/dawmii/moreupgrades/MoreUpgradesClient.java
@@ -0,0 +1,9 @@
+package net.dawmii.moreupgrades;
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreUpgradesClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/dawmii/moreupgrades/MoreUpgradesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.dawmii.moreupgrades;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MoreUpgradesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
